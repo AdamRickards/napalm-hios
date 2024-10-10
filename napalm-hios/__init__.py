04fbfc3ad43469133101995ff6a2447bc455107e
Adam <adam_rickards@hotmail.com>
2024-10-10 14:31:58 +1100

    Delete napalm-hios directory
    changed folder structure due to typo

--- napalm-hios/__init__.py
+++
@@ -1,9 +0,0 @@
-from napalm.base.base import NetworkDriver
-from napalm_hios.hios import HIOSDriver
-
-__all__ = ['HIOSDriver']
-
-def get_network_driver(name):
-    if name == "hios":
-        return HIOSDriver
-    raise NotImplementedError(f'Driver not found: {name}')
--- napalm-hios/utils.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-def log_error(logger, message):
-    """
-    Utility function to log errors consistently across the driver.
-    """
-    logger.error(message)
