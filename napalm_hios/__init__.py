57b8a62d9bb25c6e0a379a72a8ac37b2876b7b20
Adam <adam_rickards@hotmail.com>
2024-10-10 17:56:58 +1100

    Adding fixes for PyPi publishing
    needed to create version.py and include it in init.py

--- napalm_hios/__init__.py
+++ napalm_hios/__init__.py
@@ -1,7 +1,8 @@
 from napalm.base.base import NetworkDriver
 from napalm_hios.hios import HIOSDriver
+from napalm_hios.version import __version__
 
-__all__ = ['HIOSDriver']
+__all__ = ['HIOSDriver', '__version__']
 
 def get_network_driver(name):
     if name == "hios":
